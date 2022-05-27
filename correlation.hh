4d54be4c5cebcb2982132eb8f648c9ed5f10c04d
Tredici <marcolucio1999@gmail.com>
2022-05-27 17:26:20 +0200

    Parametrize code to supports types other than double

--- correlation.hh
+++ correlation.hh
@@ -17,12 +17,15 @@ namespace math::statistics
     // results of calculus of Pearson Correlation
     // Coefficient on large datasets:
     //  https://en.wikipedia.org/wiki/Pearson_correlation_coefficient
+    template <typename T = double>
     struct pcc_partial {
-        pcc_partial() = default;
-        pcc_partial(const pcc_partial&) = default;
-        pcc_partial(pcc_partial&&) = default;
+        using value_type = T;
 
-        auto& operator+=(const pcc_partial& p) {
+        pcc_partial() = default;
+        pcc_partial(const pcc_partial<value_type>&) = default;
+        pcc_partial(pcc_partial<value_type>&&) = default;
+
+        auto& operator+=(const pcc_partial<value_type>& p) {
             this->count += p.count;
             this->sum_1 += p.sum_1;
             this->sum_2 += p.sum_2;
@@ -32,7 +35,7 @@ namespace math::statistics
             return *this;
         }
 
-        auto operator+(const pcc_partial& p) const {
+        auto operator+(const pcc_partial<value_type>& p) const {
             auto ans = *this;
             ans += p;
             return ans;
@@ -51,7 +54,7 @@ namespace math::statistics
         }
 
         // Fastest way to add two more elements
-        auto& accumulate(double v_1, double v_2) {
+        auto& accumulate(value_type v_1, value_type v_2) {
             this->sum_1 += v_1;
             this->sum_2 += v_2;
             this->sum_1_squared += v_1*v_1;
@@ -62,17 +65,17 @@ namespace math::statistics
         }
 
         long long count{};
-        double sum_1{};
-        double sum_2{};
-        double sum_1_squared{};
-        double sum_2_squared{};
-        double sum_prod{};
+        value_type sum_1{};
+        value_type sum_2{};
+        value_type sum_1_squared{};
+        value_type sum_2_squared{};
+        value_type sum_prod{};
     };
 
-    template <typename T>
-    pcc_partial pearson_correlation_coefficient(const std::vector<T>& v1, const std::vector<T>& v2) {
+    template <typename T, typename R = T>
+    pcc_partial<R> pearson_correlation_coefficient(const std::vector<T>& v1, const std::vector<T>& v2) {
         using namespace std::literals;
-        pcc_partial ans;
+        pcc_partial<R> ans;
         if (v1.size() != v2.size()) {
             throw std::invalid_argument("Arguments must have the same length, found len(v1)="s + std::to_string(v1.size()) + ", len(v2)=" + std::to_string(v2.size()));
         }
@@ -92,10 +95,13 @@ namespace math::statistics
     // This class has been conceived to easily
     // calculate PCC on all pairs of columns in
     // a large dataset
+    template <typename T = double>
     class multicolumn_pcc_accumulator {
+    public:
+        using value_type = T;
     private:
         const int N;
-        std::valarray<pcc_partial> partials;
+        std::valarray<pcc_partial<value_type>> partials;
     public:
         multicolumn_pcc_accumulator(int N)
         : N{N}, partials((N-1)*N/2)
@@ -106,7 +112,7 @@ namespace math::statistics
             }
         }
 
-        auto& accumulate(const std::vector<double>& row) {
+        auto& accumulate(const std::vector<value_type>& row) {
             if (row.size() != N) {
                 using namespace std::literals;
                 throw std::runtime_error("Wrong number of columns received, expected "s + std::to_string(N) + " found "s + std::to_string(row.size()));
@@ -143,7 +149,7 @@ namespace math::statistics
         // return a map containing all
         auto results() const {
             // use un'ordered map to sped up data access
-            std::map<std::pair<int,int>,double> ans;
+            std::map<std::pair<int,int>,value_type> ans;
             auto partial_iterator = std::begin(partials);
             for (int position{}, i{}; i!=N-1; ++i) {
                 for (int j{i+1}; j!=N; ++j, ++position) {
