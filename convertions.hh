a5b3bf783bf30d5c4394d8b49a0551cf471943e2
Tredici <marcolucio1999@gmail.com>
2022-07-16 13:04:55 +0200

    Adapt code to be compliant with C++14

--- convertions.hh
+++ convertions.hh
@@ -10,63 +10,65 @@
  * functions to cast std::string
  * to numbers.
  */
-namespace math::convertions {
+namespace math {
+    namespace convertions {
 
-    template <typename T>
-    inline T ston(const std::string& s) {
-        return (T)s;
-    }
-
-    template <>
-    inline long double ston(const std::string& s) {
-        return std::stold(s);
-    }
-
-    template <>
-    inline double ston(const std::string& s) {
-        return std::stod(s);
-    }
-
-    template <>
-    inline float ston(const std::string& s) {
-        return std::stof(s);
-    }
-
-    template <>
-    inline int ston(const std::string& s) {
-        return std::stoi(s);
-    }
-
-    template <>
-    inline unsigned int ston(const std::string& s) {
-        auto cast = std::stol(s);
-        if (cast != (unsigned int)cast) {
-            using namespace std::literals;
-            throw std::out_of_range("cannot cast '"s + s + "' to unsigned int"s);
+        template <typename T>
+        inline T ston(const std::string& s) {
+            return (T)s;
         }
-        return cast;
-    }
 
-    template <>
-    inline long ston(const std::string& s) {
-        return std::stol(s);
-    }
+        template <>
+        inline long double ston(const std::string& s) {
+            return std::stold(s);
+        }
 
-    template <>
-    inline unsigned long ston(const std::string& s) {
-        return std::stoul(s);
-    }
+        template <>
+        inline double ston(const std::string& s) {
+            return std::stod(s);
+        }
 
-    template <>
-    inline long long ston(const std::string& s) {
-        return std::stoll(s);
-    }
+        template <>
+        inline float ston(const std::string& s) {
+            return std::stof(s);
+        }
 
-    template <>
-    inline unsigned long long ston(const std::string& s) {
-        return std::stoull(s);
-    }
+        template <>
+        inline int ston(const std::string& s) {
+            return std::stoi(s);
+        }
 
-} // namespace math::convertions
+        template <>
+        inline unsigned int ston(const std::string& s) {
+            auto cast = std::stol(s);
+            if (cast != (unsigned int)cast) {
+                using namespace std::literals;
+                throw std::out_of_range("cannot cast '"s + s + "' to unsigned int"s);
+            }
+            return cast;
+        }
+
+        template <>
+        inline long ston(const std::string& s) {
+            return std::stol(s);
+        }
+
+        template <>
+        inline unsigned long ston(const std::string& s) {
+            return std::stoul(s);
+        }
+
+        template <>
+        inline long long ston(const std::string& s) {
+            return std::stoll(s);
+        }
+
+        template <>
+        inline unsigned long long ston(const std::string& s) {
+            return std::stoull(s);
+        }
+
+    } // namespace convertions
+} // namespace math
 
 #endif
